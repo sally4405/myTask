0a728eb90afa3c02219f9ca5ac6542db691fb344
sungju-kim <wnwn0719@gmail.com>
2022-05-11 14:33:16 +0900

    주석제거

--- MyTask/MyTask/ViewController.swift
+++ MyTask/MyTask/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  MyTask
-//
-//  Created by dale on 2022/05/11.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
