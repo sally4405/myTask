5556b37b95c49f392d0378a3e29816013966d4c9
Lee SeEun <sally4405@naver.com>
2022-05-11 15:43:42 +0900

    [#4] Feat: TaskCell 구현

---
+++ MyTask/MyTask/View/Extention/UIFont+Extention.swift
@@ -0,0 +1,10 @@
+import UIKit.UIFont
+
+extension UIFont {
+    static let superLargeRegular = UIFont.systemFont(ofSize: 32, weight: .regular)
+    static let largeBold = UIFont.systemFont(ofSize: 18, weight: .bold)
+    static let semiLargeBold = UIFont.systemFont(ofSize: 16, weight: .bold)
+    static let mediumRegular = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let mediumBold = UIFont.systemFont(ofSize: 14, weight: .bold)
+    static let smallRegular = UIFont.systemFont(ofSize: 12, weight: .regular)
+}
--- MyTask/MyTask/View/TaskCell.swift
+++ MyTask/MyTask/View/TaskCell.swift
@@ -1,5 +1,62 @@
 import UIKit
 
 class TaskCell: UICollectionViewCell {
+    private var title: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .semiLargeBold
+        label.text = "GitHub 공부하기"
+        return label
+    }()
 
+    private var contents: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .mediumRegular
+        label.text = "add, commit, push"
+        return label
+    }()
+
+    private var author: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .smallRegular
+        label.textColor = .systemGray4
+        label.text = "author by sally"
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layout()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.layout()
+    }
+}
+
+private extension TaskCell {
+    func layout() {
+        let inset: CGFloat = 8
+        self.addSubview(title)
+        self.addSubview(contents)
+        self.addSubview(author)
+
+        NSLayoutConstraint.activate([
+            self.title.topAnchor.constraint(equalTo: self.topAnchor, constant: inset * 2),
+            self.title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: inset * 2),
+            self.title.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -inset * 2),
+
+            self.contents.topAnchor.constraint(equalTo: self.title.bottomAnchor, constant: inset),
+            self.contents.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: inset * 2),
+            self.contents.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -inset * 2),
+
+            self.author.topAnchor.constraint(equalTo: self.contents.bottomAnchor, constant: inset),
+            self.author.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: inset * 2),
+            self.author.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -inset * 2),
+            self.author.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -inset * 2)
+        ])
+    }
 }
